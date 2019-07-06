e0da04e0565d8f74761a994267ad88bf4d599183
Leticia Moura <letcia.msilva@gmail.com>
2019-07-06 11:12:31 -0300

    Arquivo criacao de Indice ajeitado

--- src/indiceHASHPython/criaIndice.py
+++ src/indiceHASHPython/criaIndice.py
@@ -3,7 +3,7 @@ import hashlib
 import os
  
 hashSize = 900001
-fileName = "data/201901_BolsaFamilia.csv"
+fileName = "data/201801_BolsaFamilia.csv"
 indexName = "data/bolsa-hash.dat"
 # dataFormat = "72s72s72s72s2s8s2s"
 indexFormat = "14sLL"
@@ -39,6 +39,8 @@ actualLine = 1
 
 lines = f.readlines(buffer)
 line = lines.append(actualLine)
+recordNumber = 0
+
 # while True:
 while (actualLine < len(lines)-1):
     line = lines[actualLine]
@@ -47,22 +49,24 @@ while (actualLine < len(lines)-1):
 
     record = str(line).split(';') 
     p = h(record[keyColumnIndex]) 
-    po = record[keyColumnIndex] #para debug somente
+    
    # pi = h(record[keyColumnIndex])
-    fi.seek(p*indexStruct.size)
+    fi.seek(p*indexStruct.size, os.SEEK_SET)
     indexRecord = indexStruct.unpack(fi.read(indexStruct.size))  # pegar Nis
-    fi.seek(p*indexStruct.size)
+    fi.seek(p*indexStruct.size, os.SEEK_SET)
 
+    #if indexRecord[0][0] == "\0":
+   
     if indexRecord[0] == indexStruct.unpack(indexStruct.pack(b'',0,0))[0]:
-        fi.write(indexStruct.pack(record[keyColumnIndex].encode(), p, 0))
+        fi.write(indexStruct.pack(record[keyColumnIndex].encode(), recordNumber, 0))
     else:
             nextPointer = indexRecord[2]
             fi.write(indexStruct.pack(indexRecord[0], indexRecord[1],fileIndexSize))
             fi.seek(0,2) # os.SEEK_END Fim do arquivo
-            fi.write(indexStruct.pack(record[keyColumnIndex], p, nextPointer))
+            fi.write(indexStruct.pack(record[keyColumnIndex].encode(), recordNumber, nextPointer))
             fileIndexSize = fi.tell()
 
-     # recordNumber += 1
+    recordNumber += 1
     actualLine += 1
     p += len(line) + 1
    
