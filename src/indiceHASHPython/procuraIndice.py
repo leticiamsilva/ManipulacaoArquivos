dbd2d0f90ad7ac74f0671afd7bb95be61736b9cd
Leticia Moura <letcia.msilva@gmail.com>
2019-07-06 13:44:23 -0300

    Arquivo procurar indice via hash ajeitado

--- src/indiceHASHPython/procuraIndice.py
+++ src/indiceHASHPython/procuraIndice.py
@@ -4,8 +4,8 @@ import os
 import sys
  
 hashSize = 900001
-fileName = "data/201901_BolsaFamilia.csv"
-fileAComparar = "data/201902_BolsaFamiliaT.csv"
+fileName = "data/201801_BolsaFamilia.csv"
+fileAComparar = "data/201901_BolsaFamilia.csv"
 indexName = "data/bolsa-hash.dat"
 indexFormat = "14sLL"
 keyColumnIndex = 5
@@ -15,39 +15,41 @@ buffer = 50000
 indexStruct = struct.Struct(indexFormat)
   
 def h(key):
-    global hashSize
     return int(hashlib.sha1(key.encode('utf-8')).hexdigest(),16)%hashSize
  
-f = open(fileName,"rb")
 
 f2 = open(fileAComparar,"rb")
 lines = f2.readlines(buffer)
-linesAComparar = 0
+linesAComparar = 1 #ignorar header
 
 while (linesAComparar < len(lines)-1):
-    nis = str(lines[linesAComparar]).split(';')[keyColumnIndex] # o keycolumnIndex da linha atual, no caso o nis atual
+    ln = str(lines[linesAComparar]).split(';')
+    nis = ln[keyColumnIndex] # o keycolumnIndex da linha atual, no caso o nis atual
     # nis = sys.argv[1]
-    
+         
+    f = open(fileName,"rb")
     fi = open(indexName,"r+b")
+
     p = h(nis)
     offset = p*indexStruct.size
     i = 1
+     
 
     while True:
-        fi.seek(offset)
+        fi.seek(offset, os.SEEK_SET)
         indexRecord = indexStruct.unpack(fi.read(indexStruct.size))
+        compareIndex = indexRecord[0].decode('UTF-8')[0:11] #pega nis
 
-        line = f.readline()
-        if indexRecord[0] == nis:
-            f.seek(indexRecord[1])
-            record = str(line).split(';')
-            print (record[4]) #nome
+        if compareIndex == nis:
+            f.seek(indexRecord[1], os.SEEK_SET)            
+            record = ln #linha do nis
+            print (ln[6]) #nome
             print (i)
             break
         offset = indexRecord[2]
-        print(offset)
         if offset == 0:
             break
-        i += 1
+        print (i)
+        i +=1
     linesAComparar +=1
 fi.close()
